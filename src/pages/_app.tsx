0bfeccde5ed232dbff4eb15b4925fc527c15bba5
Fernando Silva <fernando457829@gmail.com>
2022-01-17 19:51:01 -0300

    feat: remove unnecessary code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-}
+};
 
-export default MyApp;
+export default App;
