d3db4305111526e95af147da1670e4d180edfdb5
Fernando Silva <fernandonsilva16@gmail.com>
2022-09-02 22:22:44 -0300

    fix(images): inline url

--- src/chess/images.tsx
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = 'https://images.chesscomfiles.com/chess-themes';
-
-export const PIECES_BASE_URL = `${BASE_URL}/pieces/neo/150`;
-
-export const BOARD_IMAGE_URL = `${BASE_URL}/boards/green/150.png`;
